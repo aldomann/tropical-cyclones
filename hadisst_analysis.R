4cecbee16c20dc27fa0a42aa0b8b43430dbc0686
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-02 15:16:30 +0200

    Clean comments & added NetCDF data URL

--- hadisst_analysis.R
+++ hadisst_analysis.R
@@ -5,18 +5,18 @@ library(hadsstr)
 library(tidyverse)
 library(lubridate)
 
-# Read raw data --------------------------------------------
+# Read and clean raw data ----------------------------------
 
-# Open file
+# hadsst.url <- paste0("http://www.metoffice.gov.uk/hadobs/hadisst/data/", "HadISST_sst.nc.gzt")
 hadsst.raster <- load_hadsst(file = "/home/aldomann/Downloads/Hadley/HadISST_sst.nc")
 
-# Get annual and mean SSTs:
+# Get annual and mean SSTs
 startYear <- 1966;
 endYear <- 2007;
 annual.ssts.raster <- get_annual_ssts(hadsst.raster, years = startYear:endYear)
 mean.sst.raster <- get_average_sst(annual.ssts.raster, years = startYear:endYear)
 
-# Crop SST data to a certain region:
+# Crop SST data to a certain region/basin
 startLat <- -105;
 endLat <- 20;
 startLon <- 0;
@@ -24,11 +24,11 @@ endLon <- 60;
 annual.ssts.raster <- crop(annual.ssts.raster, extent(startLat, endLat, startLon, endLon))
 mean.sst.raster <- crop(mean.sst.raster, extent(startLat, endLat, startLon, endLon))
 
-# Get mean SST of the whole region:
+# Get mean SST of the whole basin
 mean.sst.df <- data.frame( rasterToPoints( mean.sst.raster) )
 mean.sst <- mean(mean.sst.df$layer)
 
-# Get annual SST mean of the whole region:
+# Get annual SST mean of the whole basin
 annual.ssts.df <- data.frame( t( rasterToPoints( annual.ssts.raster) ) )
 annual.ssts.df <- data.frame(sst = rowMeans(annual.ssts.df[-(1:2),]))
 annual.ssts.df <- annual.ssts.df %>%
@@ -37,13 +37,13 @@ annual.ssts.df <- annual.ssts.df %>%
 	mutate(sst.norm = sst/mean.sst)
 annual.ssts.df <- annual.ssts.df[c("year", "sst", "sst.norm")]
 
-# Separation by SST:
+# Separation by SST
 annual.ssts.df <- annual.ssts.df %>%
 	mutate(sst.class = ifelse(sst.norm >= 1, "hight", "low"))
 
 # Data visualisation ---------------------------------------
 
-# Plot time series using ggplot:
+# Plot time series
 ggplot(annual.ssts.df) +
 	labs(title = paste0("N. Atl. SST between ", startYear, "-", endYear ),
 			 x = "Time (year)", y = "SST/⟨SST⟩") +
