adfbcb0d1c6139f0dd77b6c5c93898d8d1793df7
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-02 15:08:00 +0200

    Added separation by SST

--- hadisst_analysis.R
+++ hadisst_analysis.R
@@ -11,7 +11,7 @@ library(lubridate)
 hadsst.raster <- load_hadsst(file = "/home/aldomann/Downloads/Hadley/HadISST_sst.nc")
 
 # Get annual and mean SSTs:
-startYear <- 1931;
+startYear <- 1966;
 endYear <- 2007;
 annual.ssts.raster <- get_annual_ssts(hadsst.raster, years = startYear:endYear)
 mean.sst.raster <- get_average_sst(annual.ssts.raster, years = startYear:endYear)
@@ -37,12 +37,16 @@ annual.ssts.df <- annual.ssts.df %>%
 	mutate(sst.norm = sst/mean.sst)
 annual.ssts.df <- annual.ssts.df[c("year", "sst", "sst.norm")]
 
+# Separation by SST:
+annual.ssts.df <- annual.ssts.df %>%
+	mutate(sst.class = ifelse(sst.norm >= 1, "hight", "low"))
+
 # Data visualisation ---------------------------------------
 
 # Plot time series using ggplot:
 ggplot(annual.ssts.df) +
-	labs(title = paste0("NAtl SST between ", startYear, "-", endYear ),
-			 x = "Time (year)", y = "SST/<SST>") +
+	labs(title = paste0("N. Atl. SST between ", startYear, "-", endYear ),
+			 x = "Time (year)", y = "SST/⟨SST⟩") +
  	geom_line(aes(x = year, y = sst.norm), colour = "red") +
-	geom_point(aes(x = year, y = sst.norm), colour = "red") +
+	geom_point(aes(x = year, y = sst.norm, colour = sst.class)) +
 	geom_hline(aes(yintercept=1), color = "green")
