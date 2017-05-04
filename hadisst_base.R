ce828ba6f8af9a59cfcc1ee6416bc4310618300a
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-04 04:31:11 +0200

    Manually implement hadsstR functions (they will need to be modified)

--- hadisst_base.R
+++ hadisst_base.R
@@ -1,12 +1,41 @@
 # Base code to study the SST data from the Hadley Centre (HadISST)
 # Author: Alfredo Hernández
 
-# library(devtools)
-# install_github("jebyrnes/hadsstR")
-library(hadsstr)
 library(raster) # Crop raster data
 library(tidyverse)
 library(lubridate)
+
+# hadsstR functions ----------------------------------------
+# library(devtools)
+# install_github("jebyrnes/hadsstR")
+# library(hadsstr)
+
+load_hadsst <- function(file = "./HadISST_sst.nc") {
+	b <- brick(file)
+	NAvalue(b) <- -1000
+	return(b)
+}
+
+get_annual_ssts <- function(hadsst_raster, years = 1969:2011) {
+	mean_rasts <-
+		apply(matrix(years), 1, function(x) {
+			yearIDx <- which(chron::years(hadsst_raster@z$Date) == x)
+			subset_x <- subset(hadsst_raster, yearIDx)
+			means <- calc(subset_x, mean, na.rm = TRUE)
+			names(means) <- as.character(x)
+			return(means)
+		})
+	mean_brick <- brick(mean_rasts)
+	mean_brick <- setZ(mean_brick, as.Date(paste0(years, '-01-01')), 'Date')
+	return(mean_brick)
+}
+
+get_average_sst <- function(hadsst_raster, years = 1969:2011) {
+	yearIDs <- which(chron::years(hadsst_raster@z$Date) %in% years)
+	subset_x <- subset(hadsst_raster, yearIDs)
+	average_sst <- calc(subset_x, mean, na.rm = TRUE)
+	return(average_sst)
+}
 
 # Read and clean raw data ----------------------------------
 
@@ -15,6 +44,7 @@ hadsst.raster <- load_hadsst(file = "/home/aldomann/Downloads/Hadley/HadISST_sst
 
 # Get annual and mean SSTs
 # start.year <- 1966; end.year <- 2007;
+
 annual.ssts.raster <- get_annual_ssts(hadsst.raster, years = start.year:end.year)
 mean.sst.raster <- get_average_sst(annual.ssts.raster, years = start.year:end.year)
 
