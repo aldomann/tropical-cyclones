5dbf5b2ca5e339490144a3fce96e802d1856649a
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-10 13:24:20 +0200

    Map tests with ggalt

--- hurdat2_maps.R
+++ hurdat2_maps.R
@@ -3,47 +3,34 @@
 
 source("hurdat2_base.R")
 
-# Data visualisation -------------------------------------------------
+# Data visualisation ---------------------------------------
 
 # US East States Map Data
-east_states <- c("florida", "georgia", "south carolina",
-								 "north carolina", "virginia", "maryland",
-								 "delaware", "new jersey", "new york",
-								 "connecticut", "massachusetts",
-								 "rhode island", "vermont", "new hampshire",
-								 "maine", "pennsylvania", "west virginia",
-								 "tennessee", "kentucky", "alabama",
-								 "arkansas", "texas", "mississippi",
-								 "louisiana")
-east_us <- map_data("state", region = east_states)
-world_map <- map_data("world2", region = ".")
+east.states <- c("florida", "georgia", "south carolina", "north carolina", "virginia", "maryland",
+								 "delaware", "new jersey", "new york", "connecticut", "massachusetts",
+								 "rhode island", "vermont", "new hampshire", "maine", "pennsylvania", "west virginia",
+								 "tennessee", "kentucky", "alabama", "arkansas", "texas", "mississippi",  "louisiana")
+east.us <- map_data("state", region = east.states)
 
-ggplot(world_map, aes(x = long, y = lat, group = group)) +
+ggplot(east.us, aes(x = long, y = lat, group = group)) +
 	geom_polygon(fill = "cornsilk", color = "cornsilk") +
 	theme_void() +
 	xlim(c(-108, -65)) + ylim(c(23, 48)) +
-	geom_path(data = hurr_obs,
-						aes(x = -longitude, y = latitude,
-								group = storm_id),
+	geom_path(data = hurr.obs, aes(x = -longitude, y = latitude, group = storm.id),
 						color = "red", alpha = 0.2, size = 0.2) +
-	# geom_path(data = filter(hurr_obs, cat_5),
-	# 					aes(x = -longitude, y = latitude,
-	# 							group = storm_id),
-	# 					color = "red") +
 	facet_wrap(~ decade)
 
-
 # Function to track a single hurricane
-map_track <- function(storm, year, map_data = east_us, hurr_data = hurr_obs){
-	to_plot <- hurr_obs %>%
-		filter(storm_name == toupper(storm) & storm_year == year)
-	out <- ggplot(east_us, aes(x = long, y = lat, group = group)) +
+map_track <- function(storm, year, map_data = east.us, hurr_data = hurr.obs){
+	to.plot <- hurr.obs %>%
+		filter(storm.name == toupper(storm) & storm.year == year)
+	out <- ggplot(east.us, aes(x = long, y = lat, group = group)) +
 		geom_polygon(fill = "cornsilk") +
 		theme_void() +
 		xlim(c(-108, -65)) + ylim(c(23, 48)) +
-		geom_path(data = to_plot,
+		geom_path(data = to.plot,
 							aes(x = -longitude, y = latitude, group = NULL)) +
-		geom_point(data = to_plot,
+		geom_point(data = to.plot,
 							 aes(x = -longitude, y = latitude, group = NULL,
 							 		color = status, size = wind), alpha = 0.5)
 	return(out)
@@ -51,9 +38,25 @@ map_track <- function(storm, year, map_data = east_us, hurr_data = hurr_obs){
 
 map_track(storm = "Katrina", year = "2005")
 
-# Empty map
-world_map <- map_data("world", region = ".")
-ggplot(world_map, aes(x = long, y = lat, group = group)) +
-	geom_polygon(fill = "blue", color = "cornsilk") +
-	theme_void() +
-	xlim(c(-8, 180)) + ylim(c(-90, 90))
+
+# Test maps ------------------------------------------------
+
+# Running in dev mode
+# devtools::dev_mode(on=T)
+# # install_github("hadley/ggplot2")
+# # install_github("hrbrmstr/ggalt")
+#
+# library(maps)
+# library(ggplot2)
+# library(ggalt)
+#
+# world_map <- map_data("world")
+# world_map <- subset(world_map, region!="Antarctica")
+#
+# gg <- ggplot(data = world_map, aes(x = long, y = lat, group = group)) +
+# 	geom_cartogram(map = world_map, aes(map_id = region))
+#
+# gg + xlim(c(-180, -90)) + ylim(c(5, 25)) + coord_proj("+proj=merc") +
+# 	geom_path(data = hurr.obs, aes(x = long.num, y = lat.num, group = storm.id), color = "red", alpha = 0.2, size = 0.2)
+#
+# devtools::dev_mode(on=F)
