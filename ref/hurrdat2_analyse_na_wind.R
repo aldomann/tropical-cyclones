1313e1182a0a7070e545209ab52d5054b95716a0
Alfredo Hernández <aldomann.designs@gmail.com>
2017-05-10 13:33:44 +0200

    minor update

--- ref/hurrdat2_analyse_na_wind.R
+++ ref/hurrdat2_analyse_na_wind.R
@@ -1,14 +1,14 @@
 # Clean up wind column -------------------------------------
 
 # Get storms_ids of storms with wind = -99
-hurr_na_wind <- hurr_obs %>%
+hurr_na_wind <- hurr.natl.obs %>%
 	dplyr::filter(as.numeric(wind) == -99)
 
-na_list <- c(unique(hurr_na_wind$storm_id))
+na_list <- c(unique(hurr_na_wind$storm.id))
 
 for(i in 1:length(na_list)){
-	hurr_new <- hurr_obs %>%
-		dplyr::filter(storm_id == as.character(na_list[i]))
+	hurr_new <- hurr.natl.obs %>%
+		dplyr::filter(storm.id == as.character(na_list[i]))
 	print(na_list[i])
 	print(hurr_new$wind)
 }
